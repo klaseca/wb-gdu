e14605e20e0cdafcc9d7a765c3cf85f2ea85d6db
Maxim Molochkov <klaseca@gmail.com>
2020-03-31 13:00:58 +0400

    Added animation to list paths

--- src/components/Settings/PathsList/PathsList.jsx
+++ src/components/Settings/PathsList/PathsList.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import s from './PathsList.module.css';
 
+import { useTransition, animated } from 'react-spring';
+
 import PathItem from './../PathItem/PathItem';
 
 export default function PathsList({ paths }) {
+  const notPath = useTransition(paths.length === 0, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
+  const pathItem = useTransition(paths, item => item.id, {
+    from: {
+      transform: 'translate3d(0, -40px, 0)',
+      opacity: 0
+    },
+    enter: {
+      transform: 'translate3d(0, 0px, 0)',
+      opacity: 1
+    },
+    leave: {
+      transform: 'translate3d(0, -40px, 0)',
+      opacity: 0,
+      height: 0
+    }
+  });
+
   return paths.length === 0 ? (
-    <div className={s.notBox}>Not paths</div>
+    notPath.map(
+      ({ item, props, key }) =>
+        item && (
+          <animated.div style={props} key={key} className={s.notBox}>
+            Not paths
+          </animated.div>
+        )
+    )
   ) : (
     <>
-      {paths.map(({ id, path }) => (
-        <PathItem key={id} path={path} id={id} />
+      {pathItem.map(({ item, props, key }) => (
+        <animated.div style={props} key={key}>
+          <PathItem   
+            path={item.path}
+            id={item.id}
+          />
+        </animated.div>
       ))}
     </>
   );
