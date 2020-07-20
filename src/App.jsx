00780e144ea5adef5211bac99f1d75d5b2ecc8a6
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 00:48:41 +0400

    Add Redux and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,56 @@ import Main from './pages/Main/Main';
 import Settings from './pages/Settings/Settings';
 import Toast from 'com/Common/Toast';
 
-import store from 'store/store';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import {
+  setShowSelectBlock,
+  setIsAutoUpdate,
+} from 'app/store/main/mainActions';
 
-const App = () => {
+import { initSettings } from 'store/settings/settingsActions';
+
+import { useMountEffect } from 'app/hooks/useMountEffect';
+
+import sh from 'app/utils/settings-handler';
+
+const App = ({ setIsAutoUpdate, setShowSelectBlock, initSettings }) => {
+  useMountEffect(() => {
+    const effect = async () => {
+      const settings = await sh.getSettings();
+
+      initSettings(settings);
+
+      if (settings.defaultUpdateType === 'auto') {
+        setIsAutoUpdate(true);
+        setShowSelectBlock();
+      } else if (settings.defaultUpdateType === 'manual') {
+        setIsAutoUpdate(false);
+        setShowSelectBlock();
+      }
+    };
+    effect();
+  });
+
   return (
-    <Provider store={store}>
-      <Router>
-        <Toast />
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Main} />
-          <Route path='/settings' component={Settings} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Toast />
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route path='/settings'>
+          <Settings />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
-export default App;
+const mapDispatchToProps = {
+  setIsAutoUpdate,
+  setShowSelectBlock,
+  initSettings,
+};
+
+export default connect(null, mapDispatchToProps)(App);
