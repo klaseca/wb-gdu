62cc2e7854cbc8b9eac72272fcc3b12f88f9a7f3
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 00:47:49 +0400

    Add Redux store provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
 
+import { Provider } from 'react-redux';
+import store from 'store/store';
+
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
 
 serviceWorker.unregister();
