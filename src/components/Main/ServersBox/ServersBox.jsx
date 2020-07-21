b663ce135ae5ff1c421907fb400d73181c201708
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 14:21:29 +0400

    Delete settings-handler

--- src/components/Main/ServersBox/ServersBox.jsx
+++ src/components/Main/ServersBox/ServersBox.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import s from './ServersBox.module.css';
 
-import { useMountEffect } from 'app/hooks/useMountEffect';
-
 import { FormGroup, FormControlLabel } from '@material-ui/core';
 import CustomCheckbox from 'com/Common/CustomCheckbox';
 
 import { connect } from 'react-redux';
-import { setStateCheckbox, enableCheckbox } from 'app/store/settings/settingsActions';
+import { setStateCheckbox } from 'app/store/settings/settingsActions';
 
-import sh from 'app/utils/settings-handler';
-
-function ServersBox({ servers, setStateCheckbox, enableCheckbox }) {
-  useMountEffect(() => {
-    async function ue() {
-      const settings = await sh.read();
-
-      settings.servers.forEach(({ id, checked }) => {
-        if (checked) enableCheckbox(id);
-      });
-    }
-    ue();
-  });
-
+function ServersBox({ servers, setStateCheckbox }) {
   return (
     <div className={s.box}>
       <FormGroup row>
@@ -51,7 +36,6 @@ function ServersBox({ servers, setStateCheckbox, enableCheckbox }) {
 const mapStateToProps = ({ settings: { servers } }) => ({ servers });
 const mapDispatchToProps = {
   setStateCheckbox,
-  enableCheckbox
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ServersBox);
