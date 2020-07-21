27f36584c501bbd88290b6571a336402b4c3157c
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 16:26:12 +0400

    Delete settings-handler and Redux actions

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,32 +1,12 @@
 import React from 'react';
 import s from './Main.module.css';
 
-import { useMountEffect } from 'app/hooks/useMountEffect';
-
 import UpdateOptions from 'com/Main/UpdateOptions/UpdateOptions';
 import Updater from 'com/Main/Updater/Updater';
 
 import { connect } from 'react-redux';
-import { setShowSelectBlock, setIsAutoUpdate } from 'app/store/main/mainActions';
 
-import sh from 'app/utils/settings-handler';
-
-function Main({ showBlock, setIsAutoUpdate, setShowSelectBlock }) {
-  useMountEffect(() => {
-    const ue = async () => {
-      const { defaultUpdateType } = await sh.read();
-
-      if (defaultUpdateType === 'auto') {
-        setIsAutoUpdate(true);
-        setShowSelectBlock();
-      } else if (defaultUpdateType === 'manual') {
-        setIsAutoUpdate(false);
-        setShowSelectBlock();
-      }
-    };
-    ue();
-  });
-
+function Main({ showBlock }) {
   return (
     <div className={s.app}>
       <div className={s.h1}>Warface bot</div>
@@ -39,9 +19,4 @@ function Main({ showBlock, setIsAutoUpdate, setShowSelectBlock }) {
 
 const mapStateToProps = ({ main: { showBlock } }) => ({ showBlock });
 
-const mapDispatchToProps = {
-  setIsAutoUpdate,
-  setShowSelectBlock
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default connect(mapStateToProps)(Main);
