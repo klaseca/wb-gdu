1a9d04a073af5f25216d781ad41340392ce1d010
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 16:09:04 +0400

    Delete settings-handler and Redux state

--- src/components/Settings/PathItem/PathItem.jsx
+++ src/components/Settings/PathItem/PathItem.jsx
@@ -5,44 +5,21 @@ import { IconButton } from '@material-ui/core';
 import { Close } from '@material-ui/icons';
 
 import { connect } from 'react-redux';
-import { setPaths } from 'app/store/settings/settingsActions';
+import { removePath } from 'app/store/settings/settingsActions';
 
-import sh from 'app/utils/settings-handler';
-
-function PathItem({ id, path, setPaths, paths, isSinglePaths }) {
-  const multiRemovePath = async id => {
-    const newPaths = paths.filter(path => path.id !== id);
-
-    setPaths(newPaths);
-
-    const settings = await sh.read();
-
-    if (isSinglePaths) {
-      settings.singlePaths = newPaths;
-    } else {
-      settings.multiPaths = newPaths;
-    }
-
-    await sh.save(settings);
-  };
-
+function PathItem({ id, path, removePath }) {
   return (
     <div className={s.item}>
       <div className={s.itemText}>{path}</div>
-      <IconButton size='small' color='inherit' onClick={() => multiRemovePath(id)}>
+      <IconButton size='small' color='inherit' onClick={() => removePath(id)}>
         <Close fontSize='small' />
       </IconButton>
     </div>
   );
 }
 
-const mapStateToProps = ({ settings: { paths, isSinglePaths } }) => ({
-  paths,
-  isSinglePaths
-});
-
 const mapDispatchToProps = {
-  setPaths
+  removePath,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PathItem);
+export default connect(null, mapDispatchToProps)(PathItem);
