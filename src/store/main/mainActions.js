61066b09f135fa4325047e206baa93a5959d2ac6
Maxim Molochkov <klaseca@gmail.com>
2020-07-21 13:45:44 +0400

    Add update result functionality

--- src/store/main/mainActions.js
+++ src/store/main/mainActions.js
@@ -1,41 +1,60 @@
 export const CHANGE_VALUE = 'CHANGE_VALUE';
-export const CHANGE_STATE_CHECKBOX = 'CHANGE_SERVER_CHECKBOX';
 export const SHOW_SELECT_BLOCK = 'SHOW_SELECT_BLOCK';
 export const CHANGE_AUTO_UPDATE = 'IS_AUTO_UPDATE';
 export const CHANGE_IS_TOAST = 'CHANGE_IS_TOAST';
 export const CHANGE_TOAST_DATA = 'CHANGE_TOAST_DATA';
 export const CHANGE_IS_UPDATING = 'CHANGE_IS_UPDATING';
+export const CHANGE_IS_UPDATED = 'CHANGE_IS_UPDATED';
+export const CHANGE_UPDATE_RESULT = 'CHANGE_UPDATE_RESULT';
 
 export const setValue = ({ target: { value } }) => ({
   type: CHANGE_VALUE,
-  payload: value
+  payload: value,
 });
 
 export const resetValue = () => ({
   type: CHANGE_VALUE,
-  payload: ''
+  payload: '',
 });
 
 export const setShowSelectBlock = () => ({
-  type: SHOW_SELECT_BLOCK
+  type: SHOW_SELECT_BLOCK,
 });
 
-export const setIsAutoUpdate = isAutoUpdate => ({
+export const setIsAutoUpdate = (isAutoUpdate) => ({
   type: CHANGE_AUTO_UPDATE,
-  payload: isAutoUpdate
+  payload: isAutoUpdate,
 });
 
-export const setIsToast = bool => ({
+export const setIsToast = (bool) => ({
   type: CHANGE_IS_TOAST,
-  payload: bool
+  payload: bool,
 });
 
-export const setToastData = data => ({
+export const setToastData = (data) => ({
   type: CHANGE_TOAST_DATA,
-  payload: data
+  payload: data,
 });
 
-export const setIsUpdating = bool => ({
+export const setIsUpdating = (bool) => ({
   type: CHANGE_IS_UPDATING,
-  payload: bool
+  payload: bool,
+});
+
+export const setIsUpdated = (bool) => ({
+  type: CHANGE_IS_UPDATED,
+  payload: bool,
+});
+
+export const setUpdateResult = (payload) => ({
+  type: CHANGE_UPDATE_RESULT,
+  payload,
+});
+
+export const resetUpdateResult = () => ({
+  type: CHANGE_UPDATE_RESULT,
+  payload: {
+    successResult: [],
+    failResult: [],
+  },
 });
--- src/store/main/mainReducers.js
+++ src/store/main/mainReducers.js
@@ -4,58 +4,75 @@ import {
   CHANGE_AUTO_UPDATE,
   CHANGE_IS_TOAST,
   CHANGE_TOAST_DATA,
-  CHANGE_IS_UPDATING
+  CHANGE_IS_UPDATING,
+  CHANGE_IS_UPDATED,
+  CHANGE_UPDATE_RESULT,
 } from './mainActions';
 
 const defaultState = {
   value: '',
   showBlock: true,
   isAutoUpdate: true,
-  isToast: false,
   isUpdating: false,
+  isUpdated: false,
+  successResult: [],
+  failResult: [],
+  isToast: false,
   toastData: {
     text: '',
-    severity: ''
-  }
+    severity: '',
+  },
 };
 
 const handlers = {
   [CHANGE_VALUE](state, value) {
     return {
       ...state,
-      value
+      value,
     };
   },
   [SHOW_SELECT_BLOCK](state) {
     return {
       ...state,
-      showBlock: !state.showBlock
+      showBlock: !state.showBlock,
     };
   },
   [CHANGE_AUTO_UPDATE](state, isAutoUpdate) {
     return {
       ...state,
-      isAutoUpdate
-    }
+      isAutoUpdate,
+    };
   },
   [CHANGE_IS_TOAST](state, isToast) {
     return {
       ...state,
-      isToast
-    }
+      isToast,
+    };
   },
   [CHANGE_TOAST_DATA](state, toastData) {
     return {
       ...state,
-      toastData
-    }
+      toastData,
+    };
   },
   [CHANGE_IS_UPDATING](state, isUpdating) {
     return {
       ...state,
-      isUpdating
-    }
-  }
+      isUpdating,
+    };
+  },
+  [CHANGE_IS_UPDATED](state, isUpdated) {
+    return {
+      ...state,
+      isUpdated,
+    };
+  },
+  [CHANGE_UPDATE_RESULT](state, payload) {
+    return {
+      ...state,
+      ...payload,
+    };
+  },
 };
 
 export const mainReducer = (state = defaultState, action) => {
